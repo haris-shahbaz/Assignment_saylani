f2f5ccc5255876ed82e1ae837fafd329e4cb7846
unknown <harisshahbaz707@gmail.com>
2023-09-01 07:50:08 -0700

    upgating on git

---
+++ app.js
@@ -0,0 +1,76 @@
+
+// Chapter No 1
+
+
+// Question # 1 and 2
+// alert ("Please enter a valid password");
+
+
+// Question # 3
+// alert ("Welcome to js land... \n Happy Coading!");
+
+
+// Question # 4
+// alert ("Welcome to js land...");
+// alert ("Happy Coading");
+
+
+// Question # 5
+// alert ("Hello... I can run JS through my web browser's console");
+// console.log ("Hello... I can run JS through my web browser's console");
+
+
+
+
+
+// Chapter No 2
+
+
+// Question # 1
+// var Username = "Haris";
+// console.log (Username);
+
+
+// Question # 2
+// var myname = "Haris Shahbaz";
+// console.log (myname);
+
+
+
+// Question # 3
+// var message = "Hello World";
+// alert (message);
+
+
+// Question # 4
+// var studentname = "jhone Doe";
+// var age = "15 years old";
+// var certified = "Certified Mobile Application development"
+// alert (studentname);
+// alert (age);
+// alert (certified);
+
+
+// Question # 5
+// var alerts = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert (alerts);
+
+
+// Question # 6
+// var address = "Your Email Address is ";
+// var email = "harishahbaz707@gmail.com";
+// alert (address + email);
+
+
+// Question # 7
+// var book = "A smarter way to learn Javascript";
+// alert ("I am trying to learn from the book " + book);
+
+
+// Question # 8
+// document.write ("Yeh! I can right HTML through Javascript");
+
+
+// Question # 9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert (design);
